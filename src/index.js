ee292be5da71c0bce9753190a73367fad770193d
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-01-05 11:42:49 -0800

    Get rid of magic value of 10. Count number of correct and incorrect answers.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
+const numberOfQuestions = 3;
+
 function getRandomInteger() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
 console.log(getRandomInteger());
+let correctAnswerCount = 0;
+let incorrectAnswerCount = 0;
 
-const num1 = getRandomInteger();
-const num2 = getRandomInteger();
-const answer = parseInt(prompt(`Ile jest ${num1} * ${num2} ?`));
-const result = num1 * num2;
-console.log(num1, num2, result, answer);
+for (let i = 0; i <numberOfQuestions; i++) {
+    const num1 = getRandomInteger();
+    const num2 = getRandomInteger();
+    const answer = parseInt(prompt(`Ile jest ${num1} * ${num2} ?`));
+    const result = num1 * num2;
 
-if (answer === result) {
-    alert('poprawnie');
-} else {
-    alert('błąd');
+    console.log(num1, num2, result, answer);
+
+    if (answer === result) {
+        correctAnswerCount++;
+    }
 }
-
-
+incorrectAnswerCount = numberOfQuestions - correctAnswerCount;
+console.log(correctAnswerCount);
+console.log(incorrectAnswerCount);
 
 
 
