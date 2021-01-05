db2403dc6fff1cc37684ca3e55955955debce692
Marek Gniady <marek.j.gniady@gmail.com>
2021-01-05 19:40:03 +0100

    Implement function generating random integers in range 1 to 10 (inclusive)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-const answer = prompt('Ile jest 7 * 3 ?');
+function getRandomInteger(){
+    return Math.floor(Math.random()*10)+1;
+}
+console.log(getRandomInteger());
+// const answer = prompt('Ile jest 7 * 3 ?');
+
