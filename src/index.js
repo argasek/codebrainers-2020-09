fecd1ee1aed1e191b46187595a7b7c52e66f59a6
Paulina Murzanska <p.murzanska@gmail.com>
2021-01-05 20:13:44 +0100

    Generate two random integers, multiply them and verify answer correctness.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-function getRandomInteger(){
-    return Math.floor(Math.random()*10)+1;
+function getRandomInteger() {
+    return Math.floor(Math.random() * 10) + 1;
 }
+
 console.log(getRandomInteger());
-// const answer = prompt('Ile jest 7 * 3 ?');
+
+const num1 = getRandomInteger();
+const num2 = getRandomInteger();
+const answer = parseInt(prompt(`Ile jest ${num1} * ${num2} ?`));
+const result = num1 * num2;
+console.log(num1, num2, result, answer);
+
+if (answer === result) {
+    alert('poprawnie');
+} else {
+    alert('błąd');
+}
+
+
+
+
+
 
