ee41ae492aea7b060d4dea9c15cc93ce5aa704dd
Jakub Argasiński <jakub@argasinski.eu>
2021-01-07 18:38:46 +0100

    Exercise 01 - task 1 solution

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ function getRandomInteger() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
-console.log(getRandomInteger());
 let correctAnswerCount = 0;
 let incorrectAnswerCount = 0;
 
 for (let i = 0; i <numberOfQuestions; i++) {
     const num1 = getRandomInteger();
     const num2 = getRandomInteger();
-    const answer = parseInt(prompt(`Ile jest ${num1} * ${num2} ?`));
-    const result = num1 * num2;
 
-    console.log(num1, num2, result, answer);
+    let answer;
+
+    do {
+        answer = parseInt(prompt(`Ile jest ${num1} * ${num2} ?`));
+    } while(isNaN(answer));
+
+    const result = num1 * num2;
 
     if (answer === result) {
         correctAnswerCount++;
     }
 }
 incorrectAnswerCount = numberOfQuestions - correctAnswerCount;
-console.log(correctAnswerCount);
-console.log(incorrectAnswerCount);
-
 
 function isExamPassed(total, answerCount) {
     return answerCount >= total * 0.6;
