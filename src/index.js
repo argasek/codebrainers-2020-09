07d363cc63e6d91fd10b76bc9ae4ed69b3416c5c
Jakub Argasiński <jakub@argasinski.eu>
2021-01-07 20:24:09 +0100

    Exercise 02 - introduction to array iteration methods

--- src/index.js
+++ src/index.js
@@ -1,58 +1,34 @@
-const numberOfQuestions = 3;
+const names = [ 'Basia', 'Adrian', 'Jan', 'Paulina', 'Jakub', 'Artur', 'Damian', 'Hermenegilda' ];
 
-function getRandomInteger() {
-    return Math.floor(Math.random() * 10) + 1;
+console.log('------------- for loop -------------------');
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
 }
 
-let answers = [];
-let questions = [];
+console.log('------------- loop via forEach and anonymous function -------------------');
 
-for (let i = 0; i < numberOfQuestions; i++) {
-    const num1 = getRandomInteger();
-    const num2 = getRandomInteger();
+names.forEach(function (value) {
+    console.log(value);
+});
 
-    const question = [ num1, num2 ];
-    questions.push(question);
+console.log(names);
 
-    let answer;
+console.log('------------- filter() demonstration -------------------');
 
-    do {
-        answer = parseInt(prompt(`Ile jest ${ num1 } * ${ num2 } ?`));
-    } while (isNaN(answer));
+const onlyNamesLongerThan5Letters = names.filter(function (name) {
+    return name.length === 5;
+});
 
-    answers.push(answer);
-}
+console.log(onlyNamesLongerThan5Letters);
 
-function isExamPassed(questionsAskedCount, correctAnswerCount) {
-    return correctAnswerCount >= questionsAskedCount * 0.6;
-}
+console.log('------------- map() demonstration -------------------');
 
-function isAnswerCorrect(a, b, c) {
-    return c === a * b;
-}
+const mapper = function (name) { return `${name} Nowak`; };
+const printer = function (fullName) { console.log(fullName); };
 
-const comment = [];
-let questionCorrectness = [];
+// chain
+// method chaining
+const namesWithNowakFullNames = names.map(mapper).forEach(printer);
 
-for (let i = 0; i < questions.length; i++) {
-    const a = questions[i][0];
-    const b = questions[i][1];
-    const answer = answers[i];
-    const result = isAnswerCorrect(a, b, answer);
-    questionCorrectness.push(result);
-    comment.push(`User asked if ${a} * ${b} = ${answer} said it was ${result}`);
-}
-
-function identity(value) {
-    return value;
-}
-
-const onlyCorrectAnswersArray = questionCorrectness.filter(identity);
-const correctAnswerCount = onlyCorrectAnswersArray.length;
-
-let textToShow = comment.join("\n");
-
-textToShow += '\nDid user pass the exam? ' + isExamPassed(questions.length, correctAnswerCount);
-
-alert(textToShow);
-
+console.log(namesWithNowakFullNames)
