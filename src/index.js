39f1425f927b0bc50b49fac8f457a95ed1026de1
Paulina Murzanska <p.murzanska@gmail.com>
2021-01-07 21:06:47 +0100

    Match names list to surname, use modulo operator.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-const names = [ 'Basia', 'Adrian', 'Jan', 'Paulina', 'Jakub', 'Artur', 'Damian', 'Hermenegilda' ];
-const surnames= ['Kowalsk', 'Nowak', 'Pawlak', 'Kwiatkowsk'];
+const names = ['Basia', 'Adrian', 'Jan', 'Paulina', 'Jakub', 'Artur', 'Damian', 'Hermenegilda', 'Konstancja'];
+const surnames = ['Kowalsk', 'Nowak', 'Pawlak', 'Kwiatkowsk'];
 const flexTable = [true, false, false, true];
 
-const filteredNames = names.filter(function (value) {
-    return value.length > 5;
-});
-
+// const filteredNames = names.filter(function (value) {
+//     return value.length > 5;
+// });
+const filteredNames = names;
 console.log(names, filteredNames);
+
+const fullNames = filteredNames.map(function (name, index) {
+    const fullName = name + " " + surnames[index % surnames.length];
+    // console.log(name, index, surnames[index % surnames.length] , index % surnames.length);
+
+
+    return fullName;
+})
+
+console.log(fullNames);
