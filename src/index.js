89700a268ab2f5cceef652a1ccc9f2a2664da2c6
Damian Glapa <damian3681@gmail.com>
2021-01-07 20:35:43 +0100

    Filter names longer than 5 characters.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 const names = [ 'Basia', 'Adrian', 'Jan', 'Paulina', 'Jakub', 'Artur', 'Damian', 'Hermenegilda' ];
+const surnames= ['Kowalsk', 'Nowak', 'Pawlak', 'Kwiatkowsk'];
+const flexTable = [true, false, false, true];
 
+const filteredNames = names.filter(function (value) {
+    return value.length > 5;
+});
+
+console.log(names, filteredNames);
