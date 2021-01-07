f36894004fded33b0db19991a20c7001406b3b26
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-01-07 14:13:44 -0800

    Task 1 - Modify mapper()

--- src/index.js
+++ src/index.js
@@ -8,16 +8,22 @@ const surnames = ['Kowalsk', 'Nowak', 'Pawlak', 'Kwiatkowsk'];
 const flexTable = [true, false, false, true];
 
 function filter(value) {
-    return value.length > 5;
+    return value.length > 2;
 }
 
 const filteredNames = names.filter(filter);
 
-// console.log(names, filteredNames);
-
 const mapper = function (name, index) {
-    const fullName = name + " " + surnames[index % surnames.length];
-    // console.log(name, index, surnames[index % surnames.length] , index % surnames.length);
+    let fullName;
+
+    if (name.endsWith('a') && name !== 'Jarema' && name !== 'Barnaba'
+        && flexTable[index % surnames.length]) {
+        fullName = name + " " + surnames[index % surnames.length] + "a";
+    } else if (flexTable[index % surnames.length]) {
+        fullName = name + " " + surnames[index % surnames.length] + "i";
+    } else {
+        fullName = name + " " + surnames[index % surnames.length];
+    }
 
     return fullName;
 };
