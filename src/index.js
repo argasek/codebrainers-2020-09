0fe6bd3e9d767bde26cf3c936406e4aee95ea345
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-01-09 07:50:40 -0800

    Implement reverse string function.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const text = 'Ala ma kota, a kot ma psa';
 
 const reverse = x => {
     let invertedString = '';
-    for (let i = x.length - 1; x === 0; i--) {
+    for (let i = x.length - 1; i >= 0; i--) {
         invertedString += x[i];
     }
     return invertedString;
