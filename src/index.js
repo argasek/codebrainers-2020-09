31a0a489f49f6f4d4c933b416b74aad66af8242c
Damian Glapa <damian3681@gmail.com>
2021-01-11 23:00:25 +0100

    Function to calculate time of working hours

--- src/index.js
+++ src/index.js
@@ -11,38 +11,59 @@ const employeeWorkingHours = [
 
 const timeRangeToMinutes = (text) => {
     let minutes = 0;
-    let x = text.split('-');
+    const x = text.split('-');
 
     const earlierTimeStamp = x[0];
     const laterTimeStamp = x[1];
+
     const earlierHoursWithMinutes = earlierTimeStamp.split(':');
     const laterHoursWithMinutes = laterTimeStamp.split(':');
-    const earlierHours = parseInt(earlierHoursWithMinutes[0])*60;
-    const earlierMin = parseInt(earlierHoursWithMinutes[1]);
-    const laterHours = parseInt(laterHoursWithMinutes[0])*60;
-    const laterMin = parseInt(laterHoursWithMinutes[1]);
 
-    let earlierTime= [earlierHours, earlierMin] ;
-    let laterTime = [laterHours, laterMin];
+    const earlierHoursMin = [parseInt(earlierHoursWithMinutes[0]), parseInt(earlierHoursWithMinutes[1])];
+    const laterHoursMin = [parseInt(laterHoursWithMinutes[0]), parseInt(laterHoursWithMinutes[1])];
+    const minutesInHour = 60;
+
+    const earlierTime = earlierHoursMin[0] * minutesInHour + earlierHoursMin[1];
+    const laterTime = laterHoursMin[0] * minutesInHour + laterHoursMin[1];
 
     minutes = laterTime - earlierTime
 
     return minutes;
 
 };
-    timeRanges.reduce((hours,min) => hours * 60 + min);
+//  timeRanges.reduce((hours,min) => hours * 60 + min);
 
-
-
-const timeRange = timeRangeToMinutes('11:30-12:32');
-console.log(timeRange);
-
-
-const getWorkingTimeAnalysis = (workingHours, weeklyWorkingHours = 40) => {
-    let days = 0;
-    let hours = 0;
+const minutes = (workingTime) => {
+    let tableOfHours = [];
     let minutes = 0;
+
+    for (let i = 0; i < workingTime.length; i++) {
+        tableOfHours = workingTime[i];
+
+        for (let i = 0; i < tableOfHours.length; i++) {
+            minutes += timeRangeToMinutes(tableOfHours[i]);
+        }
+        ;
+    }
+    ;
+    return minutes;
+};
+
+
+//const timeRange = timeRangeToMinutes('11:30-12:32');
+
+const timeRange = minutes(employeeWorkingHours);
+
+const getWorkingTimeAnalysis = (workingMinutes, weeklyWorkingHours = 40) => {
+
+
+    let days = employeeWorkingHours.length;
+    let hours = workingMinutes % 480 / 60;
+    let minutes = workingMinutes % 480 % 60;
     let didWorkOvertime = false;
+    if (workingMinutes / 60 > weeklyWorkingHours ) {
+        didWorkOvertime = true;
+    }
 
     return {
         days: days,
@@ -50,10 +71,12 @@ const getWorkingTimeAnalysis = (workingHours, weeklyWorkingHours = 40) => {
         minutes: minutes,
         didWorkOvertime: didWorkOvertime,
     }
+    ;
+
 };
 
 
-const analysis = getWorkingTimeAnalysis(employeeWorkingHours)
+const analysis = getWorkingTimeAnalysis(timeRange)
 const didWorkOvertime = analysis.didWorkOvertime;
 console.log(analysis);
 console.log(`The employee spent ${analysis.days} days, ${analysis.hours} hours and ${analysis.minutes} minutes.`);
