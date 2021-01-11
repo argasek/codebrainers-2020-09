64b49dd1fe4424965a82d2cce236e127abc3a7a7
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-01-11 06:53:17 -0800

    Finished Task - implement correctly working `didWorkOvertime()`.

--- src/index.js
+++ src/index.js
@@ -8,11 +8,66 @@ const employeeWorkingHours = [
     ['00:00-06:59'],
 ];
 
+const timeRangeToMinutes = (text) => {
+    let minutes = 0;
+    let x = text.split('-');
+
+    let hoursWithMinutes = [];
+    let minutesTimeStamp = [];
+
+    const beginPeriodOfTime = 0;
+    const endPeriodOfTime = 1;
+
+    for (let i = 0; i <= 1; i++) {
+        hoursWithMinutes[i] = x[i].split(':')
+            .map(item => parseInt(item));
+
+        const hoursPosition = 0;
+        const minutesPosition = 1;
+
+        minutesTimeStamp[i] = hoursWithMinutes[i][hoursPosition] * 60
+            + hoursWithMinutes[i][minutesPosition];
+    }
+    ;
+    minutes = minutesTimeStamp[endPeriodOfTime] - minutesTimeStamp[beginPeriodOfTime];
+    return minutes;
+};
+
+const fullWeekWorkingMinutes = (employeeWeekHours) => {
+    let dayHours = [];
+    let sumOfWorkingMinutes = 0;
+
+    for (let iWeek = 0; iWeek < employeeWeekHours.length; iWeek++) {
+        dayHours = employeeWeekHours[iWeek];
+
+        for (let iDay = 0; iDay < dayHours.length; iDay++) {
+            sumOfWorkingMinutes += timeRangeToMinutes(dayHours[iDay]);
+        }
+        ;
+    }
+    ;
+    console.log(`Sum of working minutes in the week: ${sumOfWorkingMinutes}`);
+    return sumOfWorkingMinutes;
+};
+
+const weekTimeRange = fullWeekWorkingMinutes(employeeWorkingHours);
+
 const getWorkingTimeAnalysis = (workingHours, weeklyWorkingHours = 40) => {
     let days = 0;
     let hours = 0;
     let minutes = 0;
     let didWorkOvertime = false;
+
+    const dayMinutes = 480;
+    const hoursMinutes = 60;
+
+    days = parseInt(workingHours / dayMinutes);
+    hours = parseInt(workingHours % dayMinutes / hoursMinutes);
+    minutes = parseInt(workingHours % dayMinutes % hoursMinutes);
+
+    if ((workingHours / 60) > weeklyWorkingHours) {
+        didWorkOvertime = true;
+    }
 
     return {
         days: days,
@@ -22,9 +77,9 @@ const getWorkingTimeAnalysis = (workingHours, weeklyWorkingHours = 40) => {
     }
 };
 
-const analysis = getWorkingTimeAnalysis(employeeWorkingHours)
+const analysis = getWorkingTimeAnalysis(weekTimeRange);
 const didWorkOvertime = analysis.didWorkOvertime;
 
-console.log(`The employee spent ${analysis.days} days, ${analysis.hours} hours and ${analysis.minutes} minutes.`);
+console.log(`The employee spent ${analysis.days} days, ${analysis.hours} hours
+            and ${analysis.minutes} minutes.`);
 console.log(`This means that this employee ${didWorkOvertime ? 'did' : 'didn\'t'} overtime`);
-
