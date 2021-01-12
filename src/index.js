0a6de8872924232bfd3588e1e308fd5ddc6d05f2
Jakub Argasiński <jakub@argasinski.eu>
2021-01-12 21:00:04 +0100

    Exercise 04 - OOP (Object Oriented Programming) paradigms in JS - classes and objects, inheritance, instanceof etc.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,70 @@
-const employeeWorkingHours = [
-    [ '11:30-12:30', '16:00-22:00' ],
-    [ '7:15-8:30', '11:30-14:30', '16:00-23:59' ],
-    [ '00:00-01:59' ],
-    [ '6:00-23:00' ],
-    [ '9:15-10:58', '11:30-14:30', '16:00-23:59' ],
-    [ '6:00-23:00' ],
-    [ '00:00-06:59' ],
-];
+// POJO
 
-const toMinutes = hours => hours * 60;
-
-const timestampToMinutes = timestamp => timestamp
-    .split(':')
-    .map(x => parseInt(x))
-    .reduce((acc, next) => toMinutes(acc) + next);
-
-const timestampRangeToMinutes = timestampRange => timestampRange
-    .split('-')
-    .map(timestampToMinutes)
-    .reduce((a, b) => b - a);
-
-const businessDayMapReducer = businessDay => businessDay
-    .map(timestampRangeToMinutes)
-    .reduce((businessDayMinutes, timeRange) => businessDayMinutes + timeRange, 0)
-
-const getWorkingTimeAnalysis = (businessDays, weeklyWorkingHours = 40) => {
-    const totalMinutes = businessDays
-        .map(businessDayMapReducer)
-        .reduce((result, businessDayMinutes) => result + businessDayMinutes);
-
-    const minutesInAnHour = 60;
-    const minutesInOneDay = 24 * minutesInAnHour;
-
-    const maximumMinutes = weeklyWorkingHours * minutesInAnHour;
-
-    const didWorkOvertime = totalMinutes > maximumMinutes;
-
-    const days = Math.floor(totalMinutes / minutesInOneDay);
-    const hours = Math.floor((totalMinutes % minutesInOneDay) / minutesInAnHour);
-    const minutes = (totalMinutes % minutesInOneDay) % minutesInAnHour;
-
-    return {
-        days: days,
-        hours: hours,
-        minutes: minutes,
-        didWorkOvertime: didWorkOvertime,
-    };
-};
+class CarWheel {
+    constructor(id) {
+        this.id = id;
+    }
+}
 
 
-const analysis = getWorkingTimeAnalysis(employeeWorkingHours);
-const didWorkOvertime = analysis.didWorkOvertime;
 
-console.log(`The employee spent ${ analysis.days } days, ${ analysis.hours } hours and ${ analysis.minutes } minutes.`);
-console.log(`This means that this employee ${ didWorkOvertime ? 'did' : 'didn\'t' } overtime`);
+class Vehicle {
+    constructor() {
+        console.log('Vehicle constructor called')
+    }
+
+}
+
+class Car extends Vehicle {
+    constructor() {
+        super();
+        console.log('Car constructor called');
+
+        this.wheels = [
+            this.createWheel('front left'),
+            this.createWheel('front right'),
+            this.createWheel('back left'),
+            this.createWheel('back right'),
+        ];
+
+        this.spareWheels = [];
+
+        this.steeringWheel = undefined;
+    }
+
+    addSpareWheel() {
+        const spareWheel = this.createWheel('spare wheel');
+        this.spareWheels.push(spareWheel);
+    }
+
+    createWheel(id) {
+        return new CarWheel(id);
+    }
+
+    getNumberOfWheels() {
+        return this.wheels.length + this.spareWheels.length;
+    }
+
+}
+
+class SedanCar extends Car {
+
+    addSpareWheel() {
+        const spareWheel = this.createWheel('sedan spare wheel');
+        this.spareWheels.push(spareWheel);
+    }
+
+}
+
+
+const car = new Car();
+
+car.addSpareWheel();
+
+const sedanCar = new SedanCar();
+
+sedanCar.addSpareWheel();
+
+console.log(car);
+console.log(sedanCar);
 
