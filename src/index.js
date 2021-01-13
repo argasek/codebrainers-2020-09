8a21adc07af1b4ebbb1b79d3176984ddb80dac60
Jonasz Jałocha <jonasz.jalocha@gmail.com>
2021-01-13 14:07:36 -0800

    Add homework.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,9 @@ class CarWheel {
     }
 }
 
-
 class Vehicle {
     constructor() {
         console.log('Vehicle constructor called')
-    }
-
-}
-
-class Car extends Vehicle {
-    constructor() {
-        super();
-        console.log('Car constructor called');
 
         this.wheels = [
             this.createWheel('front left'),
@@ -27,7 +18,6 @@ class Car extends Vehicle {
         ];
 
         this.spareWheels = [];
-
         this.steeringWheel = undefined;
     }
 
@@ -45,11 +35,40 @@ class Car extends Vehicle {
     }
 
     exchangeWheel(wheelIndex) {
-        const wheel = this.spareWheels[0];
-        this.wheels[wheelIndex] = wheel;
-        this.spareWheels.shift();
+        if (this.spareWheels.length > 0) {
+            const wheel = this.spareWheels[0];
+            this.wheels[wheelIndex] = wheel;
+            this.spareWheels.shift();
+        } else {
+            console.log('Sorry, this car has no spare wheel(s). Add a spare wheel.');
+        }
     }
 
+    addSpareWheelFromAnotherCar(anotherCar, spareWheel) {
+        if (anotherCar instanceof Vehicle && anotherCar.spareWheels.length > 0) {
+            this.spareWheels.push(spareWheel);
+            anotherCar.spareWheels.shift();
+        } else {
+            console.log('Select another car to borrow a wheel from');
+        }
+    }
+}
+
+class Car extends Vehicle {
+    constructor() {
+        super();
+        console.log('Car constructor called');
+
+        this.wheels = [
+            this.createWheel('front left'),
+            this.createWheel('front right'),
+            this.createWheel('back left'),
+            this.createWheel('back right'),
+        ];
+
+        this.spareWheels = ['spare wheel no 1', 'spare wheel no 2'];
+        this.steeringWheel = undefined;
+    }
 }
 
 class SedanCar extends Car {
@@ -61,18 +80,15 @@ class SedanCar extends Car {
 
 }
 
-const car = new Car();
-
-car.addSpareWheel();
-
-const sedanCar = new SedanCar();
-
-sedanCar.addSpareWheel();
-
-car.exchangeWheel(1);
 
 
+const car1 = new Car();
+const sedanCar1 = new SedanCar();
 
-console.log(car);
-console.log(sedanCar);
+const car2 = car1.addSpareWheelFromAnotherCar(sedanCar1, 'sedan spare wheel')
+
+console.log(car1);
+console.log(car2);
+console.log(sedanCar1);
+
 
