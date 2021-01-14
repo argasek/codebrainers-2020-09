b304338e917d245e3f6ffbc48628cf894bc0fe97
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-01-14 06:32:23 -0800

    Homework - Task 1, 2 and 3.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// POJO
-
 class CarWheel {
     constructor(id) {
         this.id = id;
@@ -10,13 +8,6 @@ class CarWheel {
 class Vehicle {
     constructor() {
         console.log('Vehicle constructor called')
-    }
-}
-
-class Car extends Vehicle {
-    constructor() {
-        super();
-        console.log('Car constructor called');
 
         this.wheels = [
             this.createWheel('front left'),
@@ -26,13 +17,22 @@ class Car extends Vehicle {
         ];
 
         this.spareWheels = [];
-
+        this.wreckerSpareWheels = [];
         this.steeringWheel = undefined;
     }
 
     addSpareWheel() {
-        const spareWheel = this.createWheel('spare wheel ' + this.spareWheels.length);
+        const spareWheel = this.createWheel(
+            'spare wheel ' + this.spareWheels.length
+        );
         this.spareWheels.push(spareWheel);
+    }
+
+    addWreckerSpareWheel() {
+        const wreckerSpareWheel = this.createWheel(
+            'wrecker spare wheel ' + this.wreckerSpareWheels.length
+        );
+        this.wreckerSpareWheels.push(wreckerSpareWheel);
     }
 
     createWheel(id) {
@@ -43,32 +43,100 @@ class Car extends Vehicle {
         return this.wheels.length + this.spareWheels.length;
     }
 
-    exchangeWheel(wheelIndex) {
+    doExchangeWheel(wheelIndex) {
         const wheel = this.spareWheels[0];
         this.wheels[wheelIndex] = wheel;
         this.spareWheels.shift();
     }
+
+    doWreckerExchangeWheel(wheelIndex) {
+        const wreckerWheel = this.wreckerSpareWheels[0];
+        this.wheels[wheelIndex] = wreckerWheel;
+        this.wreckerSpareWheels.shift();
+    }
+
+    exchangeWheel(wheelIndex) {
+        if (this.getNumberOfWheels() <= 4) {
+            console.log('You have no spare wheel left, need to call wrecker.');
+            this.addWreckerSpareWheel();
+            this.doWreckerExchangeWheel(wheelIndex);
+        } else {
+            this.doExchangeWheel(wheelIndex);
+        }
+    }
+
 }
+
+
+class Car extends Vehicle {
+    addSpareWheel() {
+        const spareWheel = this.createWheel(
+            'car spare wheel ' + this.spareWheels.length
+        );
+        this.spareWheels.push(spareWheel);
+    }
+
+    addWreckerSpareWheel() {
+        const wreckerSpareWheel = this.createWheel(
+            'wrecker car spare wheel ' + this.wreckerSpareWheels.length
+        );
+        this.wreckerSpareWheels.push(wreckerSpareWheel);
+    }
+}
+
 
 class SedanCar extends Car {
     addSpareWheel() {
-        const spareWheel = this.createWheel('sedan spare wheel');
+        const spareWheel = this.createWheel(
+            'sedan car spare wheel ' + this.spareWheels.length
+        );
         this.spareWheels.push(spareWheel);
+    }
+
+    addWreckerSpareWheel() {
+        const wreckerSpareWheel = this.createWheel(
+            'wrecker sedan car spare wheel ' + this.wreckerSpareWheels.length
+        );
+        this.wreckerSpareWheels.push(wreckerSpareWheel);
+    }
+}
+
+
+class EstateCar extends Car {
+    addSpareWheel() {
+        const spareWheel = this.createWheel(
+            'estate car spare wheel ' + this.spareWheels.length
+        );
+        this.spareWheels.push(spareWheel);
+    }
+
+    addWreckerSpareWheel() {
+        const wreckerSpareWheel = this.createWheel(
+            'wrecker estate car spare wheel ' + this.wreckerSpareWheels.length
+        );
+        this.wreckerSpareWheels.push(wreckerSpareWheel);
     }
 }
 
 const car = new Car();
-
 car.addSpareWheel();
-
-const sedanCar = new SedanCar();
-
-sedanCar.addSpareWheel();
-
+car.exchangeWheel(2);
 car.exchangeWheel(1);
 
+const sedanCar = new SedanCar();
+sedanCar.addSpareWheel();
+sedanCar.exchangeWheel(1);
 
+const estateCar = new EstateCar();
+estateCar.addSpareWheel();
+estateCar.exchangeWheel(1);
+
+const vehicle = new Vehicle();
+vehicle.exchangeWheel(1);
+vehicle.exchangeWheel(2);
 
 console.log(car);
 console.log(sedanCar);
+console.log(estateCar);
+console.log(vehicle);
 
