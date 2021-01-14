9343e9c3622e40a9090c771d776bd2065c9612fc
Jakub Argasiński <jakub@argasinski.eu>
2021-01-14 18:02:28 +0100

    Exercise 06 - Basic React app.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
-const numberOfQuestions = 3;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-function getRandomInteger() {
-    return Math.floor(Math.random() * 10) + 1;
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-let correctAnswerCount = 0;
-let incorrectAnswerCount = 0;
-
-for (let i = 0; i < numberOfQuestions; i++) {
-    const num1 = getRandomInteger();
-    const num2 = getRandomInteger();
-
-    let answer;
-
-    do {
-        answer = parseInt(prompt(`Ile jest ${ num1 } * ${ num2 } ?`));
-    } while (isNaN(answer));
-
-    const result = num1 * num2;
-
-    if (answer === result) {
-        correctAnswerCount++;
-    }
-}
-incorrectAnswerCount = numberOfQuestions - correctAnswerCount;
-
-function isExamPassed(total, answerCount) {
-    return answerCount >= total * 0.6;
-}
-
-if (isExamPassed(numberOfQuestions, correctAnswerCount)) {
-    alert(`zdałeś`);
-} else {
-    alert(`nie zdałeś`);
-}
-
+serviceWorker.unregister();
