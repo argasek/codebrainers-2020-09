a7b0355d21c26281eb958c783e991fc331f0b70c
Paulina Murzanska <p.murzanska@gmail.com>
2021-01-16 03:11:33 -0800

    Add inline styles to <li>.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
+    const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
 
-  return (
-      <div>
-        <h1>Hej!</h1>
-        <p>Ala ma kota</p>
-          <ul>
-              {arr.map(color => (<li key={color}>{color}</li>))}
-          </ul>
-      </div>
-  );
+    return (
+        <div>
+            <h1>Hej!</h1>
+            <p>Ala ma kota</p>
+            <ul>
+                {arr.map(color =>
+                    (
+                        <li key={color} style={{color: color}}>
+                           <span style ={{paddingLeft:'30px', paddingRight:'30px', backgroundColor:'#ccc'}}> {color}</span>
+                        </li>
+                    )
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
