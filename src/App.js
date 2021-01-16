7724311aedc85ce8aa1d4a34cdd3fecd25f4cd22
Bartosz Kowalski <kowalski95.bartosz@gmail.com>
2021-01-16 04:52:29 -0800

    Refactor list of items into separate component.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.css';
 
+const listItemMapper = (color, index) =>
+    (
+        <li key={color} style={{color: color}}>
+                           <span className='list-item'
+                                 style={{paddingLeft: (30 + (index * 20)).toString() + 'px'}}> {color}
+                           </span>
+        </li>
+    );
+
+
+function ListItems(props) {
+    const listItems = props.listItems;
+    return listItems.map(listItemMapper)
+}
+
 function App() {
     const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
-
     return (
         <div>
             <h1>Hej!</h1>
             <p>Ala ma kota</p>
             <ul>
-                {arr.map((color, index) =>
-                    (
-                        <li key={color} style={{color: color}}>
-                           <span className='list-item' style ={{paddingLeft: (30+(index*20)).toString()+'px'}}> {color}</span>
-                        </li>
-                    )
-                )}
+                <ListItems listItems={arr}/>
             </ul>
         </div>
     );
