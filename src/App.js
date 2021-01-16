838916dbe10189d594a6a556367e89a8c6ff65b0
Jakub Argasiński <jakub@argasinski.eu>
2021-01-16 14:14:24 +0100

    Exercise 07 -- refactor ListItems component to make use of object destructuring

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import './App.css';
 
-const listItemMapper = (color, index) =>
-    (
-        <li key={color} style={{color: color}}>
-                           <span className='list-item'
-                                 style={{paddingLeft: (30 + (index * 20)).toString() + 'px'}}> {color}
-                           </span>
+const ListItems = ({ listItems }) => listItems.map((color, index) => {
+    const paddingLeft = (30 + (index * 20)).toString() + 'px';
+    const style = { paddingLeft };
+    return (
+        <li key={ color } style={ { color } }>
+            <span className='list-item' style={ style }> { color }</span>
         </li>
-    );
-
-
-function ListItems(props) {
-    const listItems = props.listItems;
-    return listItems.map(listItemMapper)
-}
+    )
+});
 
 function App() {
-    const arr = ['red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass'];
+    const arr = [ 'red', 'green', 'blue', 'black', 'maroon', 'teal', 'dadadass' ];
+
     return (
         <div>
             <h1>Hej!</h1>
             <p>Ala ma kota</p>
             <ul>
-                <ListItems listItems={arr}/>
+                <ListItems listItems={ arr } color={3} />
             </ul>
         </div>
     );
