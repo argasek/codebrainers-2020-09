9acfba4bd576ada3ceb9cc4fc9040aa794c13697
Jonasz Jałocha <jonasz.jalocha@gmail.com>
2021-01-17 14:36:33 -0800

    Complete Task 1 and 2. Continue Task 3 - problem with importing variables from StudentList.js to StudentRow.js.

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -15,45 +15,49 @@ function StudentList(props) {
         numberOfBeers: 'number of beers'
     };
 
-
-const sortedStudents = students.sort((student1, student2) => {
-    const a = student1[sortBy];
-    const b = student2[sortBy];
-    if (a > b) {
-        return 1 * multiplier;
-    }
-    if (b > a) {
-        return -1 * multiplier;
-    }
-    return 0;
-});
-
-function isSortingColumn (sortByColumn) {
-    if (sortByColumn === sortBy) {
-        return true;
-    }
-    return false
-};
-
-return (
-    <table className='student-table' cellSpacing="0" cellPadding="0">
-        <thead>
-        <tr>
-            <th colSpan={3} style={{background: 'teal'}} >Sorted by {sortByLabel[sortBy]}, {sortDirectionLabel}</th>
-        </tr>
-        <tr>
-            <th style={isSortingColumn('fullName') ? {background: 'teal'} : {background: '#444'}} >Full name</th>
-            <th style={isSortingColumn('numberOfBeers') ? {background: 'teal'} : {background: '#444'}}>Number of beers</th>
-            <th style={isSortingColumn('participationCount') ? {background: 'teal'} : {background: '#444'}}>Participation</th>
-        </tr>
-        </thead>
-        <tbody>
-        {
-            sortedStudents.map((student, index) => <StudentRow student={student} key={index}/>)
+    function isSortingColumnLabel(columnName) {
+        if (columnName === sortBy) {
+            return true;
         }
-        </tbody>
-    </table>
-);
+        return false;
+    };
+
+
+    const sortedStudents = students.sort((student1, student2) => {
+        const a = student1[sortBy];
+        const b = student2[sortBy];
+        if (a > b) {
+            return 1 * multiplier;
+        }
+        if (b > a) {
+            return -1 * multiplier;
+        }
+        return 0;
+    });
+
+
+    return (
+        <table className='student-table' cellSpacing="0" cellPadding="0">
+            <thead>
+            <tr>
+                <th colSpan={3} style={{background: 'teal'}}>Sorted by {sortByLabel[sortBy]}, {sortDirectionLabel}</th>
+            </tr>
+            <tr>
+                <th style={isSortingColumnLabel('fullName') ? {background: 'teal'} : {background: '#444'}}>Full name</th>
+                <th style={isSortingColumnLabel('numberOfBeers') ? {background: 'teal'} : {background: '#444'}}>Number
+                    of
+                    beers
+                </th>
+                <th style={isSortingColumnLabel('participationCount') ? {background: 'teal'} : {background: '#444'}}>Participation</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                sortedStudents.map((student, index) => <StudentRow student={student} key={index}/>)
+            }
+            </tbody>
+        </table>
+    );
 
 }
 
--- src/components/StudentRow.js
+++ src/components/StudentRow.js
@@ -1,14 +1,16 @@
 import React from "react";
-
+import '../App.css';
 
 export default function StudentRow(props) {
     const student = props.student;
     const {fullName, numberOfBeers, participationCount} = student;
+
+
     return (
         <tr>
-            <td> {fullName} </td>
-            <td> {numberOfBeers}</td>
-            <td> {participationCount}</td>
+            <td className="student-row-full-name"> {fullName} </td>
+            <td className="student-row-number-of-beers"> {numberOfBeers}</td>
+            <td className="student-row-participation"> {participationCount}</td>
         </tr>
     );
 }
