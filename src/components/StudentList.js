5ea3846883fb0436d8b73b71876c4b2efb5efc49
Bartosz Kowalski <kowalski95.bartosz@gmail.com>
2021-01-17 13:58:41 -0800

    Add colors to filter conditions. Thinking to clean code and make it more nice to Eye :)

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -4,16 +4,44 @@ import StudentRow from "./StudentRow";
 
 function StudentList(props) {
     const students = props.students;
-    const sortBy = props.sortBy;
+    let sortBy = props.sortBy;
     const sortDirection = props.sortDirection;
     const multiplier = sortDirection ? 1 : -1;
     const sortDirectionHead = sortDirection ? 'Ascending' : 'Descending';
+    let backGroundAll
+    let backGroundBeer
+    let backGroundParticipians
+    if (sortBy === "fullName"){
+        sortBy = "Student Name";
+        backGroundAll =  {backgroundColor: '#621'}
+    }
+    else if (sortBy === "numberOfBeers"){
+        sortBy = "Number of Beers";
+        backGroundBeer =  {backgroundColor: '#621'};
+    }
+
+    else {
+        sortBy = "Participation Count";
+        backGroundParticipians =  {backgroundColor: '#621'}
+    }
+ // CHcialem se tak zrobic ale cos nie bangla, a jutro robota :(
+
+        //const niceToEyeName:
+        // sortBy === "fullName" ? "Full student name" : 0 ;
+        // sortBy === "numberOfBeers" ? "Number of Beers" :0 ;
+        // sortBy === "participationCount" ? "Participation Count" : 0 ;
+
+// <th style={sortBy === "fullName" ? {backgroundColor: '#621'} : {backgroundColor: '#555'}}>Full name</th>
+// <th style={sortBy === "numberOfBeers" ? {backgroundColor: '#621'} : {backgroundColor: '#555'}}>Number of beers</th>
+// <th style={sortBy === "participationCount" ? {backgroundColor: '#621'} : {backgroundColor: '#555'}}>Participation</th>//
+//
+
 
 
     const sortedStudents = students.sort((student1, student2) => {
         const a = student1[sortBy];
         const b = student2[sortBy];
-        console.log(a,b,sortBy);
+        console.log(a, b, sortBy);
         if (a > b) {
             return 1 * multiplier;
         }
@@ -28,12 +56,15 @@ function StudentList(props) {
         <table className='student-table' cellSpacing="0" cellPadding="0">
             <thead>
             <tr>
-                <th colspan={3} style={{backgroundColor: '#555'}}>Sort by{sortBy}, Direction by: {sortDirectionHead}</th>
+                <th colspan={3} style={{backgroundColor: '#555'}}>Sort by {sortBy}, Direction
+                    by: {sortDirectionHead}</th>
             </tr>
             <tr>
-                <th>Full name</th>
-                <th>Number of beers</th>
-                <th style={{backgroundColor: '#621'}}>Participation</th>
+                <th style={backGroundAll}>Full name</th>
+                <th style={backGroundBeer}>Number of
+                    beers
+                </th>
+                <th style={backGroundParticipians}>Participation</th>
             </tr>
             </thead>
             <tbody>
