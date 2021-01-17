3dd8a504d6710572098028e37ad730b376c2b849
Paulina Murzanska <p.murzanska@gmail.com>
2021-01-17 08:13:13 -0800

    Add proper names to tables according to sorting parameter. Color chosen sorting parameter.

--- src/components/StudentRow.js
+++ src/components/StudentRow.js
@@ -3,11 +3,17 @@ import React from "react";
 export default function StudentRow(props) {
     const student = props.student;
     const {fullName, numberOfBeers, participationCount} = student;
+
+
+    let backgroundStyleCells;
+
+
+
     return (
         <tr>
-            <td> {fullName} </td>
-            <td> {numberOfBeers}</td>
-            <td> {participationCount}</td>
+            <td style={backgroundStyleCells}> {fullName} </td>
+            <td style={backgroundStyleCells}>  {numberOfBeers}</td>
+            <td style={backgroundStyleCells}> {participationCount}</td>
         </tr>
     );
 }
