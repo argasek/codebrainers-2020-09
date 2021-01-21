43accef1779aff7ca97f5eab59e3011550af70d5
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-01-21 15:43:19 +0100

    Homework with OOP - complete Task 1, 2 and 3

--- src/homework.js
+++ src/homework.js
@@ -41,10 +41,41 @@ class Vehicle {
 
     // Task 1 -- implement this method using .forEach().
     startAllEngines() {
+        const engines = this.engines;
+        engines.forEach(engine => engine.turnOn());
     }
 
     // Task 1 -- implement this method using .reduce().
     areAllEnginesRunning() {
+        const initialValue = true;
+
+        return this.engines.map(engine => engine.isRunning)
+            .reduce((enginesRunningReducer, isRunning) =>
+                enginesRunningReducer && isRunning, initialValue);
+    }
+
+    // Task 1
+    areAllEnginesStopped() {
+        const initialValue = false;
+
+        return (!this.engines.map(engine => engine.isRunning)
+            .reduce((enginesRunningReducer, isRunning) =>
+                (enginesRunningReducer || isRunning), initialValue));
+    }
+
+    // Task 2
+    isAnyEngineRunning() {
+        const initialValue = false;
+
+        return this.engines.map(engine => engine.isRunning)
+            .reduce((enginesRunningReducer, isRunning) =>
+                enginesRunningReducer || isRunning, initialValue);
+    }
+
+    // Task 3
+    areAtLeastThisMuchEnginesRunning(count) {
+        let countOfRunningEngines = this.engines.filter(engine => engine.isRunning === true);
+        return  count >= countOfRunningEngines.length;
     }
 
 }
@@ -65,19 +96,38 @@ class Airplane extends Vehicle {
             this.addEngine(airplaneEngine);
         }
     }
+
     startEngine(index) {
         const engine = this.engines[index];
         engine.turnOn();
+    }
+
+    stopEngine(index) {
+        const engine = this.engines[index];
+        engine.turnOff();
     }
 }
 
 const car = new Car();
 
 const airplane = new Airplane();
+
 airplane.startEngine(0);
 airplane.startEngine(3);
+// airplane.startAllEngines();
 
-const result = airplane.areAllEnginesRunning();
+// airplane.stopEngine(0);
+// airplane.stopEngine(1);
+// airplane.stopEngine(2);
+// airplane.stopEngine(3);
 
-console.log("Are all airplane engines running? ", result);
+const result1 = airplane.areAllEnginesRunning();
+const result2 = airplane.areAllEnginesStopped();
+const result3 = airplane.isAnyEngineRunning();
+const result4 = airplane.areAtLeastThisMuchEnginesRunning(2);
 
+console.log(airplane);
+console.log("Are all airplane engines running? ", result1);
+console.log("Are all airplane engines stopped? ", result2);
+console.log("Is any airplane engine running? ", result3);
+console.log("Are at least this much airplane's engines running? ", result4);
