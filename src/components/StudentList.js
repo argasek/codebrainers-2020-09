f9bee5862d3ebe0ad68f526289741ab290d388a2
Paulina Murzanska <p.murzanska@gmail.com>
2021-01-22 03:09:24 -0800

    Initial commit - my version of  project bind with exercise-08 original version

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -4,9 +4,31 @@ import StudentRow from "./StudentRow";
 
 function StudentList(props) {
     const students = props.students;
-    const sortBy = props.sortBy;
+    let sortBy = props.sortBy;
     const sortDirection = props.sortDirection;
     const multiplier = sortDirection ? 1 : -1;
+    const directionValue = sortDirection ? "Ascending" : "Descending";
+    let backgroundStyleName;
+    let backgroundStyleBeer;
+    let backgroundStyleAttend;
+    let checkedLabelBackground = {backgroundColor:"#621"};
+    let counter =0;
+    let sortingTitle;
+
+
+
+    if (sortBy === "fullName") {
+        sortingTitle = "student's name";
+        backgroundStyleName = checkedLabelBackground;
+
+    } else if (sortBy === "participationCount") {
+        sortingTitle = "attendance";
+        backgroundStyleAttend = checkedLabelBackground;
+
+    } else {
+        sortingTitle = "earned beers";
+        backgroundStyleBeer = checkedLabelBackground;
+    }
 
 
     const sortedStudents = students.sort((student1, student2) => {
@@ -26,24 +48,25 @@ function StudentList(props) {
         <table className='student-table' cellSpacing="0" cellPadding="0">
             <thead>
             <tr>
-                <th colSpan={3} style={{backgroundColor: '#555'}}>Sort by: "fullName", sort direction: true
-                <button>Change sort direction</button>
-                </th>
+                <th colSpan={3}>Table sorted by: {sortingTitle} | {directionValue}</th>
+
             </tr>
             <tr>
-                <th>Full name</th>
-                <th>Number of beers</th>
-                <th style={{backgroundColor: '#621'}}>Participation</th>
+                <th style={backgroundStyleName}>Full name</th>
+                <th style={backgroundStyleBeer}>Number of beers</th>
+                <th style={backgroundStyleAttend}>Participation</th>
             </tr>
             </thead>
             <tbody>
             {
-                sortedStudents.map((student, index) => <StudentRow student={student} key={index}/>)
+                sortedStudents.map((student, index) => <StudentRow student={student} key={index} sortBy={sortBy} counter={counter++} directionValue={directionValue}/>)
             }
             </tbody>
         </table>
     );
 
 }
+
+
 
 export default StudentList;
--- src/components/StudentRow.js
+++ src/components/StudentRow.js
@@ -1,13 +1,73 @@
 import React from "react";
+import {codebrainersStudents} from "../models/student";
 
 export default function StudentRow(props) {
     const student = props.student;
     const {fullName, numberOfBeers, participationCount} = student;
+    let sortBy = props.sortBy;
+    let counter = props.counter;
+    let directionValue = props.directionValue;
+    let backgroundStyleCellsName;
+    let backgroundStyleCellsBeers;
+    let backgroundStyleCellsAttendance;
+    let iterationCount = codebrainersStudents.length;
+
+    let rgba = "rgba";
+    let r = 136;
+    let g = 15;
+    let b = 15;
+    let a = changeA(counter);
+    let alterA =changeAlterA(counter);
+
+    function changeA(counter) {
+        return 0.1 +(counter/10);
+    }
+    function changeAlterA(counter){
+        return (iterationCount/10)-(counter * 0.1);
+    }
+
+    let concatenatedColor = rgba + '(' + r + ',' + g + ',' + b + ',' + a + ')';
+    let concatenatedColorAlter = rgba + '(' + r + ',' + g + ',' + b + ',' + alterA + ')';
+    let checkedCellsBackground = {backgroundColor: concatenatedColor};
+    let checkedCellsBackgroundAlter ={backgroundColor: concatenatedColorAlter};
+    console.log(checkedCellsBackgroundAlter);
+    console.log(checkedCellsBackground);
+    console.log(iterationCount);
+
+
+
+    if (sortBy === "fullName") {
+        if (directionValue === "Ascending") {
+            backgroundStyleCellsName = checkedCellsBackground;
+        } else {
+            backgroundStyleCellsName = checkedCellsBackgroundAlter;
+
+        }
+
+    } else if (sortBy === "numberOfBeers") {
+        if (directionValue === "Ascending") {
+            backgroundStyleCellsBeers = checkedCellsBackground;
+        } else {
+            backgroundStyleCellsBeers = checkedCellsBackgroundAlter;
+
+        }
+    } else if (sortBy === "participationCount") {
+        if (directionValue === "Ascending") {
+            backgroundStyleCellsAttendance = checkedCellsBackground;
+
+        } else {
+            backgroundStyleCellsAttendance = checkedCellsBackgroundAlter;
+
+        }
+    }
+
+
+
     return (
         <tr>
-            <td> {fullName} </td>
-            <td> {numberOfBeers}</td>
-            <td> {participationCount}</td>
+            <td style={backgroundStyleCellsName}> {fullName} </td>
+            <td style={backgroundStyleCellsBeers}>  {numberOfBeers}</td>
+            <td style={backgroundStyleCellsAttendance}> {participationCount}</td>
         </tr>
     );
 }
