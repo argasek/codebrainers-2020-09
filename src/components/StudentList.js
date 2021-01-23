1cfe47af45a9bc9da86693aad65ef1034f2f014c
Jonasz Jałocha <jonasz.jalocha@gmail.com>
2021-01-23 07:36:29 -0800

    Fix sortBy passing.

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,19 +1,26 @@
+
 import React from "react";
 import StudentRow from "./StudentRow";
-
 
 function StudentList(props) {
     const students = props.students;
     const sortBy = props.sortBy;
     const sortDirection = props.sortDirection;
 
-    const multiplier = (sortDirection === "ascending") ?
-        1 : (sortDirection === "descending") ? -1 : 0;
+    const multiplier = sortDirection ? 1 : -1;
+    const sortDirectionLabel = sortDirection ? "ascending" : "descending";
+
+
+    const sortByLabel = {
+        fullName: 'full name',
+        participationCount: 'participation',
+        numberOfBeers: 'number of beers'
+    };
+
 
     const sortedStudents = students.sort((student1, student2) => {
         const a = student1[sortBy];
         const b = student2[sortBy];
-
         if (a > b) {
             return 1 * multiplier;
         }
@@ -23,37 +30,40 @@ function StudentList(props) {
         return 0;
     });
 
-    const highlightStyle = {backgroundColor: '#c60707'};
-    const regularStyle = {backgroundColor: '#555'};
 
-    function highlightHeader(headerName) {
-        if (sortBy === headerName){
-            return highlightStyle;
+
+    const highlightedStyle = {background: 'teal'};
+    const normalStyle = {background: 'white'};
+
+    function isSortingColumnLabel (columnName){
+        if (columnName === sortBy)
+        {
+            return highlightedStyle;
         }
-        return regularStyle;
+        else {
+            return normalStyle;
+        }
     }
-
-    let counter = 0;
 
     return (
         <table className='student-table' cellSpacing="0" cellPadding="0">
             <thead>
             <tr>
-                <th colSpan={3} style={{backgroundColor: '#555'}}>
-                    Sort by: Student's {sortBy}.
-                    Sort direction: {sortDirection}.
-                </th>
+                <th colSpan={3} style={{background: 'teal'}}>Sorted by {sortByLabel[sortBy]}, {sortDirectionLabel}</th>
             </tr>
             <tr>
-                <th style={highlightHeader('fullName')}>Full name</th>
-                <th style={highlightHeader('numberOfBeers')}>Number of beers</th>
-                <th style={highlightHeader('participationCount')}>Participation</th>
+                <th style={isSortingColumnLabel('fullName')}>Full name
+                </th>
+                <th style={isSortingColumnLabel('numberOfBeers')}>Number
+                    of
+                    beers
+                </th>
+                <th style={isSortingColumnLabel('participationCount')}>Participation</th>
             </tr>
             </thead>
-            <tbody>
+            <tbody >
             {
-                sortedStudents.map((student, index) =>
-                    <StudentRow student={student} key={index} sortBy={sortBy} counter={counter++} />)
+                sortedStudents.map((student, index) => <StudentRow student={student} sortBy={sortBy} key={index} gradientId={index} />)
             }
             </tbody>
         </table>
@@ -61,79 +71,7 @@ function StudentList(props) {
 
 }
 
+
 export default StudentList;
-//
-//
-//
-//
-//
-//
-// import React from "react";
-// import StudentRow from "./StudentRow";
-// import '../App.css';
-//
-//
-// function StudentList(props) {
-//     const students = props.students;
-//     const sortBy = props.sortBy;
-//     const sortDirection = props.sortDirection;
-//     const multiplier = sortDirection ? 1 : -1;
-//     const sortDirectionLabel = sortDirection ? "ascending" : "descending";
-//     const sortByLabel = {
-//         fullName: 'full name',
-//         participationCount: 'participation',
-//         numberOfBeers: 'number of beers'
-//     };
-//
-//     function isSortingColumnLabel(columnName) {
-//         if (columnName === sortBy) {
-//             return true;
-//         }
-//         return false;
-//     };
-//
-//     const sortedStudents = students.sort((student1, student2) => {
-//         const a = student1[sortBy];
-//         const b = student2[sortBy];
-//         if (a > b) {
-//             return 1 * multiplier;
-//         }
-//         if (b > a) {
-//             return -1 * multiplier;
-//         }
-//         return 0;
-//     });
-//
-//
-//
-//
-//     return (
-//         <table className='student-table' cellSpacing="0" cellPadding="0">
-//             <thead>
-//             <tr>
-//                 <th colSpan={3} style={{background: 'teal'}}>Sorted by {sortByLabel[sortBy]}, {sortDirectionLabel}</th>
-//             </tr>
-//             <tr>
-//                 <th style={isSortingColumnLabel('fullName') ? {background: 'teal'} : {background: '#444'}}>Full name
-//                 </th>
-//                 <th style={isSortingColumnLabel('numberOfBeers') ? {background: 'teal'} : {background: '#444'}}>Number
-//                     of
-//                     beers
-//                 </th>
-//                 <th style={isSortingColumnLabel('participationCount') ? {background: 'teal'} : {background: '#444'}}>Participation</th>
-//             </tr>
-//             </thead>
-//             <tbody >
-//             {
-//                 sortedStudents.map((student, index) => <StudentRow student={student} key={index} gradientId={index} />)
-//             }
-//             </tbody>
-//         </table>
-//     );
-//
-// }
-//
-//
-// export default StudentList;
-//
-//
+
+
--- src/components/StudentRow.js
+++ src/components/StudentRow.js
@@ -1,89 +1,37 @@
+
 import React from "react";
 import {codebrainersStudents} from "../models/student";
 
 export default function StudentRow(props) {
     const student = props.student;
+    // const gradientId = props.gradientId+1;
     const sortBy = props.sortBy;
 
 
-    const counter = props.counter;
-    const numberOfStudents = codebrainersStudents.length;
+
+    const highlightedStyle = {background: 'teal'};
+    const normalStyle = {background: 'white'};
+
+
+
+    function isSortingColumn (columnName){
+        if (columnName === sortBy)
+        {
+            return highlightedStyle;
+        }
+        else {
+            return normalStyle;
+        }
+    }
 
     const {fullName, numberOfBeers, participationCount} = student;
 
-    function setOpacity(counter) {
-        return 100 / (numberOfStudents - 1) * counter * 0.01;
-    }
-
-    const r = 0;
-    const g = 0;
-    const b = 0;
-    const a = setOpacity(counter);
-    const opacityColor = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
-
-    const columnHighlightStyle = {backgroundColor: opacityColor, color: '#c60707'};
-    const columnRegularStyle = {backgroundColor: '#fff'};
-
-    function gradientHighlightColumn(sortByColumnName) {
-        if (sortBy === sortByColumnName) {
-            return columnHighlightStyle;
-        }
-        return columnRegularStyle;
-    }
 
     return (
         <tr>
-            <td style={gradientHighlightColumn('fullName')}> {fullName} </td>
-            <td style={gradientHighlightColumn('numberOfBeers')}> {numberOfBeers}</td>
-            <td style={gradientHighlightColumn('participationCount')}> {participationCount}</td>
+            <td style={isSortingColumn('fullName')}> {fullName} </td>
+            <td style={isSortingColumn('numberOfBeers')}> {numberOfBeers}</td>
+            <td style={isSortingColumn('participationCount')}>  {participationCount}</td>
         </tr>
     );
 }
-
-//
-// import React from "react";
-// import '../App.css';
-// import App from '../App.js';
-// // import LinearGradient from 'react-native-linear-gradient';
-//
-// export default function StudentRow(props) {
-//     const student = props.student;
-//     // const gradientColor = {
-//     //     0: 'white',
-//     //     0.1: '#d4d4d3',
-//     //     0.2: '#bfbebe',
-//     //     0.3: '#aaa9a8',
-//     //     0.4: '#959392',
-//     //     0.5: '#7f7d7c',
-//     //     0.6: '#6a6866',
-//     //     0.7: '#555251',
-//     //     0.8: '#3f3d3b',
-//     //     0.9: '#2a2725',
-//     //     1: 'black'
-//     // };
-//         let gradientColor = {
-//         0: 'white',
-//         1: '#d4d4d3',
-//         2: '#aaa9a8',
-//         3: '#7f7d7c',
-//         4: '#6a6866',
-//         5: '#555251',
-//         6: '#3f3d3b',
-//         7: 'black'
-//     };
-//     // const assumeKey=2;
-//     // const numberOfKeys = 7;
-//     const {fullName, numberOfBeers, participationCount} = student;
-//
-//
-//     return (
-//         <tr>
-//             <td style={{
-//                 background: gradientColor[1],
-//                 color: gradientColor[7]
-//             }}> {fullName} </td>
-//             <td> {numberOfBeers}</td>
-//             <td>  {participationCount}</td>
-//         </tr>
-//     );
-// }
