ba5fd737c1b0ac529097682fce6c5f1fb6ae48d2
Damian Glapa <damian3681@gmail.com>
2021-01-30 10:49:24 +0100

    Add form to Preferences.jsx.

--- src/components/preferences/Preferences.jsx
+++ src/components/preferences/Preferences.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import {Card, CardBody, Col, FormGroup, Input, Label} from "reactstrap";
 
-function Preferences (){
-  return(
-          <div>
-            <h1> Our preferences </h1>
-          </div>
+function Preferences() {
+
+  return (
+          <Card className="mb-4">
+            <CardBody>
+              <FormGroup>
+                <Label for="userFullName">Full Name:</Label>
+                <Input
+                        id="userFullName"
+                        name="userFullName"
+                        type="text"
+                        value={''}
+                        onChange={() => {
+
+                        }
+
+                        }
+                />
+              </FormGroup>
+
+            </CardBody>
+          </Card>
   )
+
 }
+
 export default Preferences;
