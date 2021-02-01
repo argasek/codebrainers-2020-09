3838e7db74602eceb67299d726d0e484a14b8c9c
Jonasz Jałocha <jonasz.jalocha@gmail.com>
2021-02-01 14:25:32 -0800

    Task 2 - change date format from ISO8601 to day/month/year hours/minutes/.

--- src/components/plants/Plant.jsx
+++ src/components/plants/Plant.jsx
@@ -11,6 +11,23 @@ const difficulties = {
   5: "high"
 }
 
+
+function dateConverter(someDate) {
+
+  if (someDate != null) {
+    let year = someDate.substring(0, 4);
+    let month = someDate.substring(5, 7);
+    let day = someDate.substring(8, 10);
+    let hour = someDate.substring(11, 13);
+    let minute = someDate.substring(14, 16);
+
+    return (day + '/' + month + '/' + year + ' ' + hour + ':' + minute);
+  } else {
+    return "no data";
+  }
+
+}
+
 class Plant extends React.PureComponent {
 
 
@@ -30,8 +47,8 @@ class Plant extends React.PureComponent {
               <td>{this.props.plant.blooming}</td>
               <td>{difficulties[this.props.plant.difficulty]}</td>
               <td>{this.props.plant.room}</td>
-              <td>{this.props.plant.last_watered}</td>
-              <td>{this.props.plant.last_fertilized}</td>
+              <td>{dateConverter(this.props.plant.last_watered)}</td>
+              <td>{dateConverter(this.props.plant.last_fertilized)}</td>
 
 
             </tr>
