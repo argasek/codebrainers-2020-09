43a4d77763b2994c23787abc66968be0fcfa04b6
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-02-02 16:01:52 +0100

    Complete Task 1, 2 and 3.

--- src/components/plants/Plant.jsx
+++ src/components/plants/Plant.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import './Plant.scss';
 import {
-  faCloud, faCloudSun, faMoon, faSun,
+  faMoon, faCloud, faCloudSun, faSun,
   faThermometerEmpty, faThermometerHalf, faThermometerFull,
   faCheckCircle, faTimesCircle,
-  faTint,
+  faTint, faTintSlash,
+  faQuestion,
 } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
@@ -51,6 +52,26 @@ const temperature = {
   warm:   <FontAwesomeIcon icon={faThermometerFull}/>,
 }
 
+
+function secondsToDays(sec) {
+  const dayInSeconds = 86400;
+  if (sec < dayInSeconds) {
+    return <FontAwesomeIcon icon={faQuestion}/>;
+  } else {
+    return Math.round(sec / dayInSeconds);
+  }
+}
+
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "long", day: "numeric" }
+  if (dateString === null) {
+    return <FontAwesomeIcon icon={faTintSlash}/>;
+  } else {
+    return new Date(dateString).toLocaleDateString(undefined, options)
+  }
+}
+
+
 class Plant extends React.PureComponent {
 
   render() {
@@ -61,16 +82,16 @@ class Plant extends React.PureComponent {
         <td>{ this.props.plant.name }</td>
         <td>{ this.props.plant.category }</td>
         <td>{ this.props.plant.category_slug }</td>
-        <td>{ this.props.plant.watering_interval }</td>
-        <td>{ this.props.plant.fertilizing_interval }</td>
+        <td>{ secondsToDays(this.props.plant.watering_interval) }</td>
+        <td>{ secondsToDays(this.props.plant.fertilizing_interval) }</td>
         <td>{ exposure[this.props.plant.required_exposure] }</td>
         <td>{ humidity[this.props.plant.required_humidity] }</td>
         <td>{ temperature[this.props.plant.required_temperature] }</td>
         <td>{ bloom_appear[this.props.plant.blooming] }</td>
         <td>{ difficulties[this.props.plant.difficulty] }</td>
         <td>{ this.props.plant.room }</td>
-        <td>{ this.props.plant.last_watered }</td>
-        <td>{ this.props.plant.last_fertilized }</td>
+        <td>{ formatDate(this.props.plant.last_watered) }</td>
+        <td>{ formatDate(this.props.plant.last_fertilized) }</td>
       </tr>
     )
   }
