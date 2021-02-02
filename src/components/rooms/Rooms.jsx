51e3f21d6fd54acb7c99ea9424b1a2bbd1931372
Marek Gniady <marek.j.gniady@gmail.com>
2021-02-02 06:31:55 -0800

    Repair fetchDelay issue, finished task 3.

--- src/components/rooms/Rooms.jsx
+++ src/components/rooms/Rooms.jsx
@@ -1,17 +1,18 @@
-import {Card, CardBody, ListGroup, Table} from "reactstrap";
+import {Card, CardBody, Table} from "reactstrap";
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import axios from "axios";
+import Room from "components/rooms/Room";
 import InProgress from "components/shared/InProgress";
-import axios from 'axios';
-import RoomItem from "./RoomItem";
-import {isPromise} from "formik";
 
 
-const ROOMS_FETCH_DELAY = 500;
+
+
+const ROOMS_FETCH_DELAY = 250;
 
 class Rooms extends React.PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       rooms: [],
       successRooms: undefined,
@@ -20,25 +21,17 @@ class Rooms extends React.PureComponent {
   }
 
   componentDidMount() {
-    this.fetchRooms()
-            .finally(() => {
-              this.setState({inProgress: false});
-            })
+    this.fetchRooms();
   }
 
   fetchRooms() {
-
-    const requestUrl = 'http://gentle-tor-07382.herokuapp.com/rooms/';
+    const requestUrl = "http://gentle-tor-07382.herokuapp.com/rooms/";
     this.setState({inProgress: true});
-
     return this.props.delayFetch(ROOMS_FETCH_DELAY, (resolve, reject) => {
-      const promise = axios.get(requestUrl)
+      const promise = axios.get(requestUrl);
 
-              promise
+      promise
               .then((response) => {
-
-                // debugger;
-
                 const data = response.data;
                 const rooms = data.map((item) => {
                   const {
@@ -46,18 +39,16 @@ class Rooms extends React.PureComponent {
                     name,
                     exposure,
                     temperature,
-                    humidity,
-                  } = item;
-                  console.log(item);
+                    humidity} = item;
+
                   return {
                     id,
                     name,
                     exposure,
                     temperature,
-                    humidity,
-                  };
-
+                    humidity};
                 });
+
                 const successRooms = true;
                 this.setState({rooms, successRooms});
                 resolve();
@@ -67,55 +58,63 @@ class Rooms extends React.PureComponent {
                 reject();
               })
               .finally(() => {
-                console.log('Resolved');
-              });
+                this.setState({inProgress: false});
+              })
     });
+
+
   }
 
   render() {
-    const {
-      inProgress,
-      successRooms,
-      rooms,
-    } = this.state;
-
+    const {rooms, successRooms, inProgress} = this.state;
 
     return (
-      <Card>
-        <CardBody>
-          <div className="app-container">
-            <InProgress inProgress={inProgress} />
-            {
-              successRooms === false &&
-              <p>Nie udało się pobrać Kategorii</p>
-            }
-            {
-              successRooms &&(
-              <Table className="categories">
-                <thead>
+            <Card>
+              <CardBody>
+
+                <InProgress inProgress={inProgress}/>
+                {
+                  successRooms === false &&
+                  <p>Unable to fetch Rooms</p>
+                }
+                {
+                  successRooms && (
+                          <>
+                            <Table>
+                            <thead>
+
                             <tr>
-                              <th>Id</th>
-                              <th>Name</th>
-                              <th>Exposure</th>
-                              <th>Temperature</th>
-                              <th>Humidity</th>
+                              <th >Id</th>
+                              <th >Room Name</th>
+                              <th >Sun exposure</th>
+                              <th >Humidity</th>
+                              <th >Temperature </th>
                             </tr>
                             </thead>
-                <tbody>
-                {
-                  rooms.map((item, index, arr) =>
-                          (<Rooms room={item} key={index}/>)
 
+                            <tbody>
+                            {
+                              rooms.map(
+                                      (room, index, arr) => (<Room room={room} key={index}/>)
+                              )
+                            }
+                            </tbody>
+
+                          </Table>
+
+                          </>
                   )
-                }</tbody>
-              </Table>
-            )}
-          </div>
-        </CardBody>
-      </Card>
+                }
+              </CardBody>
+            </Card>
     )
   }
+
+
 }
+Rooms.propTypes = {
+  delayFetch: PropTypes.func.isRequired,
+};
 
 
 export default Rooms;
