b634349dcc0fc9fffd0a90524b765b440855be1c
Damian Glapa <damian3681@gmail.com>
2021-02-02 17:42:58 +0100

    Add fields of the object representing the plant in the `<Plants>` component.

--- src/components/plants/Plant.jsx
+++ src/components/plants/Plant.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from "moment";
+
 // import PropTypes from 'prop-types';
 import './Plant.scss';
 import {faCheckSquare, faSquare, faSun, faCloudSun, faCloud, faCloudMoon,
@@ -36,11 +38,13 @@ const required_temperature={
   medium:<FontAwesomeIcon icon={faTemperatureLow}  color={"green"}/> ,
   warm: <FontAwesomeIcon icon={faTemperatureHigh}  color={"red"}/>,
 }
+/*
 const room={
   null: "N/A",
   1: <FontAwesomeIcon icon={faHome} color={"green"}/>,
   2: <FontAwesomeIcon icon={faHome} size={"2x"} color={"green"}/>,
 }
+*/
 
 
 class Plant extends React.PureComponent {
@@ -58,12 +62,12 @@ class Plant extends React.PureComponent {
         <td>{ this.props.plant.category }</td>
         <td>{ this.props.plant.category_slug }</td>
         <td>{ this.props.plant.fertilizing_interval}</td>
-        <td>{ this.props.plant.last_fertilized}</td>
-        <td>{ this.props.plant.last_watered }</td>
+        <td>{ moment(this.props.plant.last_fertilized ).format('LLLL')}</td>
+        <td>{ moment(this.props.plant.last_watered ).format('LLLL')}</td>
         <td>{ required_exposure[this.props.plant.required_exposure]}</td>
         <td>{ required_humidity[this.props.plant.required_humidity]}</td>
         <td>{ required_temperature[this.props.plant.required_temperature] }</td>
-        <td>{ room[this.props.plant.room] }</td>
+        <td>{ this.props.plant.room }</td>
         <td>{ this.props.plant.url}</td>
         <td>{ this.props.plant.watering_interval }</td>
 
